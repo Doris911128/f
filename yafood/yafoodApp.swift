438314b09265245213974daad4ca3fca84c6c546
1 <dorisspecialtopic1128@gmail.com>
2023-08-08 13:55:32 +0800

    Initial Commit

---
+++ yafood/yafoodApp.swift
@@ -0,0 +1,17 @@
+//
+//  yafoodApp.swift
+//  yafood
+//
+//  Created by Mac on 2023/8/8.
+//
+
+import SwiftUI
+
+@main
+struct yafoodApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
